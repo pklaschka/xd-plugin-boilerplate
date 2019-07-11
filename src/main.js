a3df9efe039cd903b4ed27824d51e4aca6316d94
Pablo Klaschka <contact@pabloklaschka.de>
2019-07-11 14:05:54 +0200

    Fixed many things (including typings and security alerts)

--- src/main.js
+++ src/main.js
@@ -10,19 +10,11 @@ const uxp = require('uxp');
 
 /**
  * The sample command.
- * @param {Selection} selection 
- * @param {RootNode} root 
+ * @param {Selection} selection
+ * @param {RootNode} root
  */
 async function myCommand(selection, root) {
     console.log("My Plugin");
-
-    const UXPFile = uxp.storage.File;
-    const UXPFolder = uxp.storage.Folder;
-    console.log(UXPFile, UXPFolder);
-
-    let folder = uxp.storage.localFileSystem.getDataFolder;
-
-    console.log(folder instanceof UXPFolder, folder instanceof UXPFile);
 }
 
 module.exports.commands = {
